0b66225caf4fe38c250535f87c994ecfd2bcf01d
nomy2333 <39283626+nomy2333@users.noreply.github.com>
2022-03-30 20:26:24 +1100

    Update .eslintrc.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -15,5 +15,11 @@ module.exports = {
         "react"
     ],
     "rules": {
+        // suppress errors for missing 'import React' in files
+        "react/react-in-jsx-scope": "off",
+        // allow jsx syntax in js files (for next.js project)
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }], //should add ".ts" if typescript project
+        "react/prop-types": "off",
+        "react/no-unknown-property": "off",
     }
 }
